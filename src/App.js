7c647e006e2c6dfdd3f30e3670ef4b7d2ec05c9b
shaleenjha <shaleenjha11@gmail.com>
2020-06-26 18:49:49 +0530

    Configured React

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component, version } from 'react';
+import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import './App.css';
+
+
+    class App extends Component { 
+      render() {
+        return ( 
+        <div className="App"> 
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">ConFusion</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
+        );
+      }
+    }
+  
+    export default App;
+
+
